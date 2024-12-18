38f56299bfe2c83b9b7c506d0af289ed71aa2e85
zawlinn-vlog <zawlinn.profile@outlook.com>
2024-12-18 17:57:31 +0700

    BCJ: Project 02 - Update

--- 02 Project/assets/js/app.js
+++ 02 Project/assets/js/app.js
@@ -1,46 +1,46 @@
-"use strict";
+// "use strict";
 
-function defSlide(main, parent, sub, child) {
-  $(main).siblings().children(".navbars-sub").slideUp(500);
+// function defSlide(main, parent, sub, child) {
+//   $(main).siblings().children(".navbars-sub").slideUp(500);
 
-  $(main).siblings().children(`.navbars-${parent}`).removeClass("active");
+//   $(main).siblings().children(`.navbars-${parent}`).removeClass("active");
 
-  $(main).children(`.navbars-${parent}`).addClass("active");
+//   $(main).children(`.navbars-${parent}`).addClass("active");
 
-  $(main).children(`.navbars-${sub}`).slideDown(500);
+//   $(main).children(`.navbars-${sub}`).slideDown(500);
 
-  $(main).children(`.navbars-${child}`).addClass("active");
+//   $(main).children(`.navbars-${child}`).addClass("active");
 
-  $(main).find(`.navbars-${child}:first`).addClass("active");
-}
+//   $(main).find(`.navbars-${child}:first`).addClass("active");
+// }
 
-function init() {
-  $(".navbars-sub").slideUp();
-}
+// function init() {
+//   $(".navbars-sub").slideUp();
+// }
 
-$(document).ready(function () {
-  init();
+// $(document).ready(function () {
+//   init();
 
-  $(".navbars-wrapper").click(function (e) {
-    /* Main Menu */
-    if (e.target.closest(".navbars-parent")) {
-      defSlide(e.target.closest(".navbars-menu"), "parent", "sub", "child");
+//   $(".navbars-wrapper").click(function (e) {
+//     /* Main Menu */
+//     if (e.target.closest(".navbars-parent")) {
+//       defSlide(e.target.closest(".navbars-menu"), "parent", "sub", "child");
 
-      $(".navbars-icon").removeClass("fa-chevron-up");
+//       $(".navbars-icon").removeClass("fa-chevron-up");
 
-      $(e.target).parent().children(".navbars-icon").addClass("fa-chevron-up");
+//       $(e.target).parent().children(".navbars-icon").addClass("fa-chevron-up");
 
-      // console.log("hello");
-    }
+//       // console.log("hello");
+//     }
 
-    /* SUB MENU */
+//     /* SUB MENU */
 
-    if (e.target.classList.contains("navbars-child")) {
-      $(".navbars-child").removeClass("active");
+//     if (e.target.classList.contains("navbars-child")) {
+//       $(".navbars-child").removeClass("active");
 
-      $(e.target).addClass("active");
-    }
-  });
+//       $(e.target).addClass("active");
+//     }
+//   });
 
-  // console.log($('.navbars-child'))
-});
+//   // console.log($('.navbars-child'))
+// });
